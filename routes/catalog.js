4e956ed12656cdda591abe4fe15429d1e4263318
Marek <marekkrupa03@gmail.com>
2022-11-02 23:30:59 +0100

    Testing routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const sensor_controller = require("../controllers/sensorController");
 
-router.post("/sensor", sensor_controller.sensor_create_post);
+router.get("/sensor", sensor_controller.sensor_create_post);
 
 module.exports = router;
